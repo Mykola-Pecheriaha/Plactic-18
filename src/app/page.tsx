783a71120068e6f457f8a53a22eabf83bcc0c5cd
Mykola-Pecheriaha <06072025git@gmail.com>
2025-07-29 22:53:07 +0300

    SideBySideGallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import BigBord from '@/components/BigBord/BigBord';
 import ConsultationForm from '@/components/ConsultationForm/ConsultationForm';
 import { useState } from 'react';
 import Doctor from "@/components/Doctor/Doctor"
+import SideBySideGallery from "@/components/SideBySideGallery/SideBySideGallery"
 
 export default function Home() {
   const [isConsultationFormOpen, setIsConsultationFormOpen] = useState(false);
@@ -12,6 +13,7 @@ export default function Home() {
     <>
       <BigBord onConsultationClick={() => setIsConsultationFormOpen(true)} />
         <Doctor />
+        <SideBySideGallery />
       <ConsultationForm 
         isOpen={isConsultationFormOpen} 
         onClose={() => setIsConsultationFormOpen(false)} 
